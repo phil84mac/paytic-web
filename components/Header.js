62632e6c5cc2182f9f89fad77fbefa40fc212b51
phil84mac <phil@thinkingbig.net>
2022-10-12 16:25:38 -0300

    badabing

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,92 @@
-export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+import React, { useState, useEffect, MouseEvent, Component, useRef} from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Link from 'next/link';
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+export default function Header() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <>
+    <div className="px-lg-5 w-100 d-none d-lg-block" id="home">
+      <Container fluid className="py-3 px-0">
+          <Row className="">
+            <Col xs={12} className=" d-flex align-items-center justify-content-between header-pad">
+        
+              <div className="pntr d-flex align-items-center">
+                <Link href="/">
+                  <Image fluid src="/paytic-logo-green.svg" className="w-60" alt="PayTic Logo"/>
+                </Link>
+              </div>
+                
+              <div className="d-flex align-items-center">
+                <Button href="/" variant="link" className="px-2 mx-2 text-decoration-none text-uppercase">
+                  How it works
+                </Button>
+                <Button href="/" variant="link" className="px-2 mx-2 text-decoration-none text-uppercase">
+                  Who it's for
+                </Button>
+                <Button href="/" variant="link" className="px-2 ms-2 me-4 text-decoration-none text-uppercase">
+                  About
+                </Button>
+                <Button href="/" className="text-decoration-none text-uppercase">
+                  Book a demo
+                </Button>
+              </div>
+            </Col>
+          </Row>
+      </Container>
+    </div>
+
+    <div className="px-2 w-100 d-lg-none" id="home">
+      <Container fluid className="py-3 px-0">
+          <Row className="">
+            <Col xs={12} className=" d-flex align-items-center justify-content-between header-pad">
+        
+              <div className="pntr d-flex align-items-center">
+                <Link href="/">
+                  <Image fluid src="/paytic-logo-green.svg" className="w-60" alt="PayTic Logo"/>
+                </Link>
+              </div>
+                
+              <div className="d-flex align-items-center">
+                <Button variant="link" onClick={handleShow} className="px-0">
+                  <Image fluid src="/menu-button.svg" className="w-60" alt="PayTic Logo"/>
+                </Button>
+              </div>
+            </Col>
+          </Row>
+      </Container>
+    </div>
+
+    <Offcanvas show={show} onHide={handleClose} placement="end">
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Menu</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body >
+          <div className="p-1 d-flex align-items-start flex-column bd-highlight h-100">
+                <Button href="/" variant="link" className="w-100 px-0 my-3 text-decoration-none text-uppercase border-top">
+                  How it works
+                </Button>
+                <Button href="/" variant="link" className="w-100 px-0 my-3 text-decoration-none text-uppercase border-top">
+                  Who it's for
+                </Button>
+                <Button href="/" variant="link" className="w-100 px-0 my-3 text-decoration-none text-uppercase border-top">
+                  About
+                </Button>
+                <Button href="/" className="mt-auto bd-highlight w-100 text-decoration-none text-uppercase">
+                  Book a demo
+                </Button>
+            </div>
+        </Offcanvas.Body>
+    </Offcanvas>
+
+    </>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,32 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import React, { useState, useRef, useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>PayTic Connect</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
+      <main className="px-2 px-sm-4 px-lg-5">
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+          <Container fluid className="vh-100 d-flex align-items-center px-0">
+            <Row className="vh-80">
+              <Col lg={6}>
+               <h1 className="">
+               A smarter way to manage card programs
+               </h1>
+              </Col>
+            </Row>
+          </Container>
+
       </main>
-
       <Footer />
     </div>
   )
